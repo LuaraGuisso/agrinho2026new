330b53f2373522f60a335e911a0777a94ac20e3b
LuaraGuisso <luara.guisso.lima@escola.pr.gov.br>
2026-07-07 22:52:08 -0300

    M

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function entrar() {
+
+    // Pega o nome digitado
+    let nome = document.getElementById("nome").value.trim();
+
+    // Se a pessoa não digitar nada
+    if (nome === "") {
+        nome = "Visitante";
+    }
+
+    // Salva o nome
+    localStorage.setItem("nomeUsuario", nome);
+
+    // Vai para a próxima página
     window.location.href = "informativo.html";
+
 }
